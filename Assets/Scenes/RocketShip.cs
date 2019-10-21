150754956efaf706b4a7f6565ddbb3c89b55bfd0
szn380 <joseph.dambrosio1@gmail.com>
2019-10-20 21:00:31 -0400

    add original files

---
+++ Assets/Scenes/RocketShip.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketShip : MonoBehaviour {
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();    // get access to the rigidbody component
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);   // use relative force so when rocket tilts force tilts as well
+        }
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            print("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+    }
+}
